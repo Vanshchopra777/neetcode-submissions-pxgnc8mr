f1325d8294b10131c3f7c3c62c776ec172660d69
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-04-02 18:21:10 +0530

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void dfs(int r,int c,vector<vector<int>>& nums,vector<vector<int>>& vis){
+         int n=nums.size();
+        int m=nums[0].size();
+
+        vis[r][c]=1;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+
+        for(int i=0;i<4;i++){
+            int newr=r+delrow[i];
+            int newc=c+delcol[i];
+
+            if(newr>=0 && newr<n && newc>=0 && newc<m && !vis[newr][newc] && nums[newr][newc]>=nums[r][c]){
+                
+                dfs(newr,newc,nums,vis);
+            }
+
+        }
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& nums) {
+        int n=nums.size();
+        int m=nums[0].size();
+        vector<vector<int>>pacific(n,vector<int>(m,0));
+        vector<vector<int>>atlantic(n,vector<int>(m,0));
+
+        for(int i=0;i<n;i++){
+           if(!pacific[i][0]) dfs(i,0,nums,pacific);
+            if(!atlantic[i][m-1])dfs(i,m-1,nums,atlantic);
+            
+        }
+        for(int j=0;j<m;j++){
+           if(!pacific[0][j]) dfs(0,j,nums,pacific);
+           if(!atlantic[n-1][j]) dfs(n-1,j,nums,atlantic);
+        }
+        vector<vector<int>>ans;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    ans.push_back({i,j});
+                }
+            }
+        }
+        return ans;
+
+        
+    }
+};
