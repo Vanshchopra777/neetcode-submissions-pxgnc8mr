e0c293fb1f56b335d7d07a5d6b4097574e8addce
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-04-03 14:57:18 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,47 @@
+
+
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>>adj(n);
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>indeg(n);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indeg[it]++;
+                
+            }
+        }
+        queue<int>q;
+        vector<int>topo;
+        // int count=0;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0)q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            // count++;
+
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+
+            }
+        }
+        if(topo.size()==n)return topo;
+
+        else return {};
+
+        
+        
+
+        
+    }
+};
