765818cbf5db8d94c128ad9f7c15f1e31da0e521
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-05-19 12:44:30 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+
+        for(auto it:strs){
+
+            vector<int>freq(26,0);
+
+            int n=it.size();
+            for(int i=0;i<n;i++){
+                freq[it[i]-'a']++;
+            }
+             string key="";
+            for(int i=0;i<26;i++){
+                key+=to_string(freq[i])+'#';
+
+               
+
+            }
+            mpp[key].push_back(it);
+
+
+        }
+        vector<vector<string>> ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+
+        }
+        return ans;
+    }
+};
