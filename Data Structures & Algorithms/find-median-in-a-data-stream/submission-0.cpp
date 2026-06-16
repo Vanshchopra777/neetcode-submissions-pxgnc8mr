0fb1bb299dfdf992eabfe61c7b597e6912b8078a
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-06-16 11:57:45 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+public:
+    priority_queue<int>leftmax;
+    priority_queue<int,vector<int>,greater<int>>rightmin;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+
+        if(leftmax.empty() || num<=leftmax.top()){
+            leftmax.push(num);
+        }
+        else rightmin.push(num);
+
+        if(leftmax.size()>rightmin.size()+1){
+            rightmin.push(leftmax.top());
+            leftmax.pop();
+        }
+        else if(rightmin.size()>leftmax.size()){
+            leftmax.push(rightmin.top());
+            rightmin.pop();
+        }
+        
+    }
+    
+    double findMedian() {
+        
+        if(leftmax.size()>rightmin.size())return leftmax.top();
+
+        return double(leftmax.top()+rightmin.top())/2;
+    }
+};
