0d291330c1aeffc97e0a817e087b2772f1cc963c
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-06-18 12:13:43 +0530

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int tmax=0;
+        int fresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)fresh++;
+
+
+                else if(grid[i][j]==2 && !vis[i][j]){
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
+                }
+
+
+
+            }
+        }
+        int delr[]={-1,0,1,0};
+        int delc[]={0,1,0,-1};
+
+        while(!q.empty()){
+
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int time=q.front().second;
+
+            q.pop();
+            
+            tmax=max(tmax,time);
+
+            for(int i=0;i<4;i++){
+                int newr=row+delr[i];
+                int newc=col+delc[i];
+
+                if(newr>=0 && newr<n && newc>=0 && newc<m &&
+                !vis[newr][newc] && grid[newr][newc]==1){
+                    fresh--;
+                    vis[newr][newc]=1;
+                    q.push({{newr,newc},time+1});
+                }
+            }
+
+        }
+        if(fresh!=0)return -1;
+
+        return tmax;
+        
+    }
+};
