03cd1f8820dede56196122b2af6f53fe635972b6
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-08-01 11:53:14 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.length();
+
+        int i=0;
+        int j=n-1;
+
+        while(i<=j){
+            if(isalnum(s[i]) && isalnum(s[j])){
+                if(tolower(s[i])==tolower(s[j])){
+                    i++;
+                    j--;
+                }
+                else return false;
+            }
+            else if(!isalnum(s[i]))i++;
+
+            else if(!isalnum(s[j]))j--;
+        }
+
+        return true;
+        
+    }
+};
