9603d796f088823a7f982c20153b8bc992359f83
Vanshchopra777 <vanshchopra4122@gmail.com>
2026-08-07 17:45:40 +0530

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+
+        for(auto it:strs){
+            vector<int>freq(26,0);
+            for(auto ch:it){
+                freq[ch-'a']++;
+
+
+            }
+            string key="";
+            for(int i=0;i<26;i++){
+                key+=to_string(freq[i]);
+                key+="!";
+
+            }
+            mpp[key].push_back(it);
+
+
+        }
+
+        vector<vector<string>>ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
